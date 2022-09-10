6c86c968c9d065f2b027564214d0cf94e4a2c0a3
gamename <tennis.n.smith@gmail.com>
2022-09-10 16:49:27 -0500

    handle keyboard interrupts

--- christmas.py
+++ christmas.py
@@ -17,14 +17,14 @@ colors = [
     [232, 100, 255],  # Purple
     [200, 200, 20],  # Yellow
     [30, 200, 200],  # Blue
-    [255, 255, 255], # White
-    [0, 255, 0], # Green
-    [255, 0, 0], # Red
+    [255, 255, 255],  # White
+    [0, 255, 0],  # Green
+    [255, 0, 0],  # Red
 ]
 
-max_len=5
+max_len = 5
 min_len = 2
-#pixelnum, posn in flash, flash_len, direction
+# pixelnum, posn in flash, flash_len, direction
 flashing = []
 
 num_flashes = 5
@@ -35,26 +35,29 @@ for i in range(num_flashes):
     flash_len = random.randint(min_len, max_len)
     flashing.append([pix, colors[col], flash_len, 0, 1])
 
-strip.fill((0,0,0))
+OFF = (0,0,0)
+strip.fill(OFF)
 
-while True:
-    strip.show()
-    for i in range(num_flashes):
-        #print(flashing[i])
-        pix = flashing[i][0]
-        brightness = (flashing[i][3]/flashing[i][2])
-        colr = (int(flashing[i][1][0]*brightness),
-                int(flashing[i][1][1]*brightness),
-                int(flashing[i][1][2]*brightness))
-        strip[pix] = colr
-        if flashing[i][2] == flashing[i][3]:
-            flashing[i][4] = -1
-        if flashing[i][3] == 0 and flashing[i][4] == -1:
-            pix = random.randint(0, numpix - 1)
-            col = random.randint(0, len(colors) - 1)
-            flash_len = random.randint(min_len, max_len)
-            flashing[i] = [pix, colors[col], flash_len, 0, 1]
-        flashing[i][3] = flashing[i][3] + flashing[i][4]
-    time.sleep(0.1)
-
-
+try:
+    while True:
+        strip.show()
+        for i in range(num_flashes):
+            # print(flashing[i])
+            pix = flashing[i][0]
+            brightness = (flashing[i][3] / flashing[i][2])
+            colr = (int(flashing[i][1][0] * brightness),
+                    int(flashing[i][1][1] * brightness),
+                    int(flashing[i][1][2] * brightness))
+            strip[pix] = colr
+            if flashing[i][2] == flashing[i][3]:
+                flashing[i][4] = -1
+            if flashing[i][3] == 0 and flashing[i][4] == -1:
+                pix = random.randint(0, numpix - 1)
+                col = random.randint(0, len(colors) - 1)
+                flash_len = random.randint(min_len, max_len)
+                flashing[i] = [pix, colors[col], flash_len, 0, 1]
+            flashing[i][3] = flashing[i][3] + flashing[i][4]
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    strip.fill(OFF)
+    exit()
