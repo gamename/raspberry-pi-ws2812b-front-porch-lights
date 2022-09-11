3bd638d8ccb5457685586367a3613962c07c8e9a
gamename <tennis.n.smith@gmail.com>
2022-09-11 06:12:17 -0500

    simplify

--- halloween.py
+++ halloween.py
@@ -1,23 +1,19 @@
 import board
 import neopixel
 
-from adafruit_led_animation.animation.solid import Solid
-from adafruit_led_animation.sequence import AnimationSequence
-from adafruit_led_animation.color import ORANGE
-
 # Update to match the pin connected to your NeoPixels
 pixel_pin = board.D18
 # Update to match the number of NeoPixels you have connected
 pixel_num = 24
+
+ORANGE =(253, 166, 0)
 OFF = (0, 0, 0)
 
 pixels = neopixel.NeoPixel(pixel_pin, pixel_num, brightness=0.2, auto_write=False)
-solid = Solid(pixels, color=ORANGE)
-animations = AnimationSequence(solid)
 
 try:
     while True:
-        animations.animate()
+        pixels.fill(ORANGE)
 except KeyboardInterrupt:
         pixels.fill(OFF)
         exit()
